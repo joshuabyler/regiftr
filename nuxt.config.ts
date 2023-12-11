b55cdf44ad6b00d9bc0f9adc002abb918b4a811e
Joshua Byler <bylerj@gmail.com>
2023-12-11 07:43:54 -0500

    just getting sass and everything else in place

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/ui',
+    '@nuxtjs/supabase'
+  ],
+  css: ['~/assets/styles/main.scss'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/styles/_colors.scss" as *;'
+        }
+      }
+    }
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1 class="text-3xl font-bold underline">
+    index
+    <UButton>Button</UButton>
+  </h1>
+</template>
